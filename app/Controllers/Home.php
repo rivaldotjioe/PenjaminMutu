ea4f08c7572c1b6569205cbb80c9be39c369f1bb
RivaldoTjio <rivaldohadiwinata@gmail.com>
2021-02-21 21:41:42 +0700

    cURL berhasil get link profil dosen di google scholar

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,5 +1,7 @@
 <?php namespace App\Controllers;
 
+use function Composer\Autoload\includeFile;
+
 class Home extends BaseController
 {
 
@@ -16,12 +18,18 @@ class Home extends BaseController
 	}
 
 	public function testcurl(){
-	    $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://sandbox.rachmat.id/curl/");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $output = curl_exec($ch);
-        curl_close($ch);
-        echo $output;
+        include APPPATH.'Libraries/simple_html_dom.php';
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, "https://scholar.google.com/citations?view_op=search_authors&mauthors=eka+dyar&hl=id&oi=ao");
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+         $response  = curl_exec($curl);
+         curl_close($curl);
+         $html = new \simple_html_dom();
+         $html->load($response);
+        foreach ($html->find('a[class=gs_ai_pho]') as $link){
+                echo $link->href."<br>"; //link sitasi dosen
+        }
 
     }
 
